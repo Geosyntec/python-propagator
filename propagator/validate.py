10adef5cbd82f5dd4e270cab12434ea2ed2724dd
Paul Hobson <pmhobson@gmail.com>
2015-12-22 10:10:11 -0800

    added a function to validate flow directions

--- propagator/validate.py
+++ propagator/validate.py
@@ -11,30 +11,10 @@ Written by Paul Hobson (phobson@geosyntec.com)
 
 """
 
-def direction(up_or_down):
-    """
-    Validates the direction of propation or accumulation
 
-    Parameters
-    ----------
-    up_or_down :
-        The direction in which things will move. Valid values are either
-        "upstream" or "downstream".
-
-    Raises
-    ------
-    ValueError
-        If the value of ``up_ot_down``.
-
-    Returns
-    -------
-    validated : str
-        Validated version of ``up_or_down``.
-
-    """
-
+def flow_direction(up_or_down):
     valid_directions = ['upstream', 'downstream']
     if up_or_down.lower() not in valid_directions:
-        raise ValueError("{} not one of {}".format(up_or_down, valid_directions))
+        raise ValueError("{} is not one of {}".format(up_or_down, valid_directions))
     else:
         return up_or_down.lower()
