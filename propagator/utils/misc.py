b0f758db51447bbb78acdb7c9d44ab33b08b193a
Paul Hobson <pmhobson@gmail.com>
2015-12-28 18:04:31 -0800

    function to find rows in record arrays

--- propagator/utils/misc.py
+++ propagator/utils/misc.py
@@ -88,3 +88,51 @@ def flood_zones(zones_array, topo_array, elevation):
 
     return flooded_array
 
+
+def find_row_in_array(array, column, value):
+    """
+    Find a single row in a record array.
+
+    Parameters
+    ----------
+    array : numpy.recarray
+        The record array to be searched.
+    column : str
+        The name of the column of the array to search.
+    value : int, str, or float
+        The value sought in ``column``
+
+    Raises
+    ------
+    ValueError
+        An error is raised if more than one row is found.
+
+    Returns
+    -------
+    row : numpy.recarray row
+        The found row from ``array``.
+
+    Examples
+    --------
+    >>> from propagator import utils
+    >>> import numpy
+    >>> x = numpy.array(
+            [
+                ('A1', 'Ocean', 'A1_x'), ('A2', 'Ocean', 'A2_x'),
+                ('B1', 'A1', 'None'), ('B2', 'A1', 'B2_x'),
+            ], dtype=[('ID', '<U5'), ('DS_ID', '<U5'), ('Cu', '<U5'),]
+        )
+    >>> utils.find_row_in_array(x, 'ID', 'A1')
+    ('A1', 'Ocean', 'A1_x', 'A1_y')
+
+    """
+
+    rows = filter(lambda x: x[column] == value, array)
+    if len(rows) == 0:
+        row = None
+    elif len(rows) == 1:
+        row = rows[0]
+    else:
+        raise ValueError("more than one row where {} == {}".format(column, value))
+
+    return row
